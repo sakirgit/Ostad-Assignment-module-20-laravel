b99e3286e4652caaf2458f4cc4a3bbae3aa4f5ec
Sakir Ahamed <sakiremail@gmail.com>
2023-07-19 23:51:52 +0600

    Added all files and folders

---
+++ app/Models/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Authenticatable
+{
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'full_name',
+        'email',
+        'phone',
+        'otp',
+        'password',
+    ];
+
+    protected $attributes = [
+        'otp' => '0'
+    ];
+}
